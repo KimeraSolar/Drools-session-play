eba7ec9e67697d9f2351e3ebc05fc2b0fa5b9ab9
LBBassani <lorenabassani12@gmail.com>
2022-06-15 10:46:45 -0300

    :construction: get information from fact hashString

--- src/main/java/multicast/senseup/awareness/situation/services/factServices/dummies/DummyFactFinder.java
+++ src/main/java/multicast/senseup/awareness/situation/services/factServices/dummies/DummyFactFinder.java
@@ -7,7 +7,13 @@ public class DummyFactFinder implements FactFinder {
 
     @Override
     public Fact find(String factHash) {
-        
+        String factPkg = factHash.substring(0, factHash.lastIndexOf("."));
+        String factType = factHash.substring(factHash.lastIndexOf(".") + 1, factHash.lastIndexOf("@"));
+        String hashCode = factHash.substring(factHash.lastIndexOf("@") + 1);
+
+        System.out.println("Fact Package: " + factPkg);
+        System.out.println("Fact Type: " + factType);
+        System.out.println("Fact HashCode: " + hashCode);
         System.out.println("Achamos seu fato na working memory");
         return new Fact();
     }
