58cf3d58b574aa03c363fd141aa95ef24f9dd0f1
LBBassani <lorenabassani12@gmail.com>
2022-06-15 14:58:11 -0300

    :construction: add interfaces WorkingMemoryLoader and WorkingMemorySaver

---
+++ src/main/java/multicast/senseup/awareness/situation/services/workingMemoryServices/WorkingMemorySaver.java
@@ -0,0 +1,7 @@
+package multicast.senseup.awareness.situation.services.workingMemoryServices;
+
+import multicast.senseup.awareness.situation.domain.WorkingMemory;
+
+public interface WorkingMemorySaver {
+    public void save(WorkingMemory workingMemory);
+}
