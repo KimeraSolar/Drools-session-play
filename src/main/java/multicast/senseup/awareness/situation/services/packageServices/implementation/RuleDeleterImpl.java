372b05fd1e8670672e07d7c4aaf63c1d55f711aa
LBBassani <lorenabassani12@gmail.com>
2022-06-23 17:56:20 -0300

    :ambulance: solve nullPointer when deleting rules on the middle of map

--- src/main/java/multicast/senseup/awareness/situation/services/packageServices/implementation/RuleDeleterImpl.java
+++ src/main/java/multicast/senseup/awareness/situation/services/packageServices/implementation/RuleDeleterImpl.java
@@ -1,5 +1,8 @@
 package multicast.senseup.awareness.situation.services.packageServices.implementation;
 
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
 import multicast.senseup.awareness.situation.domain.RuleForm;
 import multicast.senseup.awareness.situation.domain.RulePackage;
 import multicast.senseup.awareness.situation.domain.WorkingMemory;
@@ -18,21 +21,33 @@ public class RuleDeleterImpl implements RuleDeleter{
     public void delete(String pkgName, String ruleToDelete) {
         for(var file : workingMemory.getFiles().entrySet()){
             RulePackage rulePackage = file.getValue();
+
             if(rulePackage.getPkgName().equals(pkgName)){
-                for(var rule : rulePackage.getRules().entrySet() ){
-                    RuleForm ruleForm = rule.getValue();
+
+                var exclude = new ArrayList<> (rulePackage.getRules().entrySet().stream()
+                .filter(entry -> {
+                    RuleForm ruleForm = entry.getValue();
                     String ruleCode = ruleForm.getSourceCode();
-                    
+
                     int indexOfFirstQuote = ruleCode.indexOf("\"") + 1;
                     int indexOfSecondQuote = ruleCode.indexOf("\"", indexOfFirstQuote);
-                    if(indexOfFirstQuote == -1 || indexOfSecondQuote == -1) continue;
-                    
+                    if(indexOfFirstQuote == -1 || indexOfSecondQuote == -1) return false;
+
                     String ruleName = ruleCode.substring(indexOfFirstQuote, indexOfSecondQuote);
-                    System.out.println(ruleName);
-                    rulePackage.deleteRule(rule.getKey());
-                    PackageBuilder packageBuilder = new PackageBuilderImpl();
-                    packageBuilder.build(workingMemory, rulePackage);
-                }
+                    if (entry.getKey().equals(ruleToDelete) || ruleName.equals(ruleToDelete)){
+                        return true;
+                    }
+                    return false;
+                }).map(entry -> entry.getKey())
+                .collect(Collectors.toList()));
+                
+                if(exclude.size() == 0) continue;
+                
+                rulePackage.deleteRule(exclude.get(0));
+                PackageBuilder packageBuilder = new PackageBuilderImpl();
+                workingMemory = packageBuilder.build(workingMemory, rulePackage);
+                return;
+                
             }
         }
     }
